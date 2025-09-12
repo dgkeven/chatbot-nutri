7544700c9d0f4b083d7bbb1664076af679fe384f
Keven Mendes <keven.mendes@logone.com.br>
2025-09-12 13:50:10 -0300

    ajustes args

--- chatbot.js
+++ chatbot.js
@@ -41,7 +41,13 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-extensions',
+            '--disable-gpu',
+            '--disable-software-rasterizer'
+            ],
     }
 });
 
